56e08548b83f096d2741c90ab6760b031cacd63d
Rasmus <rasmus_skriver@yahoo.dk>
2026-04-26 18:33:01 +0200

    test

--- lua/present.lua
+++ lua/present.lua
@@ -1,1 +1,33 @@
-print "loaded present.nvim"
+local M = {}
+
+M.setup = function ()
+	--- nothing
+end
+
+---@class present.Slides
+---@fields slides string[]: The slides of the file
+
+
+--- Takes some lines and parses them
+---@param lines string[]: The lines in the buffer
+---@return present.Slides
+local parse_slides = function (lines)
+	local slides = { slides = {} }
+	local current_slide = {}
+
+	local seperator = "^#"
+	for _, line in ipairs(lines) do
+		print(line, "find:", line:find(seperator), "|")
+	end
+
+	return slides
+end
+
+vim.print(parse_slides {
+	"# Hello",
+	"this is something else",
+	"# World",
+	"this is another thing",
+})
+
+return M
